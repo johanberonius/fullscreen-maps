d6900eb98001ae010f131342e8fadbbd33ddfdb0
Johan Beronius <johan.beronius@athega.se>
2014-08-11 15:44:38 +0200

    Fix for map in iframe on 'modern' Garmin Connect

--- garmin-connect.js
+++ garmin-connect.js
@@ -1,7 +1,11 @@
-jQuery('<a class="map-control-item map-btn map-full-screen" href="#" title="Fullscreen"><span class="map-btn-icon">Fullscreen</span></a>').appendTo('.map-controls').click(function(event){
-    var m = document.getElementById('map_container');
+jQuery('<a class="map-control-item map-btn map-full-screen" href="#" title="Fullscreen"><span class="map-btn-icon">Fullscreen</span></a>')
+    .appendTo(
+        jQuery('#iFrameWidget-0').attr('allowfullscreen', 'true').contents().find('.map-controls').add('.map-controls')
+    )
+    .click(function(event) {
+    var m = this.ownerDocument.getElementById('map_container');
     m.parentElement.style.height = m.scrollHeight+'px';
-    m.style.width=m.style.height = '100%';
+    m.style.width = m.style.height = '100%';
     m.requestFullscreen = m.requestFullscreen ||
                           m.mozRequestFullScreen ||
                           m.webkitRequestFullscreen ||
