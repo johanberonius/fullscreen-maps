af7a121a0b0a69c55f27aab877286ff61701c9fa
Johan Beronius <johan.beronius@athega.se>
2015-04-29 15:00:10 +0200

    Updated Garmin Connect script to work with both classic and modern.

--- garmin-connect.js
+++ garmin-connect.js
@@ -1,15 +1,25 @@
-jQuery('<a class="map-control-item map-btn map-full-screen" href="#" title="Fullscreen"><span class="map-btn-icon">Fullscreen</span></a>')
-    .appendTo(
-        jQuery('#iFrameWidget-0').attr('allowfullscreen', 'true').contents().find('.map-controls').add('.map-controls')
-    )
-    .click(function(event) {
-    var m = this.ownerDocument.getElementById('map_container');
-    m.parentElement.style.height = m.scrollHeight+'px';
-    m.style.width = m.style.height = '100%';
-    m.requestFullscreen = m.requestFullscreen ||
-                          m.mozRequestFullScreen ||
-                          m.webkitRequestFullscreen ||
-                          m.msRequestFullscreen;
-    m.requestFullscreen();
-    event.preventDefault();
-});
+function addFullscreenButton() {
+    var mapControls = jQuery('.map-controls').eq(0).has('.map-control-item');
+
+    if (mapControls.length) {
+        var mapContainer = jQuery('#map_container, #activity-map-canvas')[0];
+        mapContainer.parentElement.style.height = mapContainer.scrollHeight+'px';
+        mapContainer.style.width = mapContainer.style.height = '100%';
+        mapContainer.requestFullscreen = mapContainer.requestFullscreen ||
+          mapContainer.mozRequestFullScreen ||
+          mapContainer.webkitRequestFullscreen ||
+          mapContainer.msRequestFullscreen;
+
+        var fullscreenButton = jQuery('<button class="map-control-item map-btn map-full-screen" title="Fullscreen"><i class="map-btn-icon icon-full-screen-2"></i></button>');
+        fullscreenButton.click(function() { mapContainer.requestFullscreen() });
+        mapControls.append(fullscreenButton);
+    } else if (--addFullscreenButton.retries)
+        setTimeout(addFullscreenButton, 1500);
+}
+
+addFullscreenButton.retries = 10;
+
+if ('jQuery' in window)
+    addFullscreenButton();
+else
+    window.addEventListener('load', addFullscreenButton);
