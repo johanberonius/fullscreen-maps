9f664fc5b6012ab32bb19390ded8f5634317ba08
Johan Beronius <johan.beronius@athega.se>
2015-04-29 15:43:20 +0200

    Added user script and fixed loading with HTTPS on Garmin Connect.

---
+++ garmin-connect/splits/user-script.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        Garmin Connect Activity Summarize Splits
+// @namespace   https://connect.garmin.com/activity
+// @include     https://connect.garmin.com/modern/activity/*
+// @include     https://connect.garmin.com/activity/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+jQuery.getScript('https://raw.githubusercontent.com/johanberonius/fullscreen-maps/master/garmin-connect/splits.js');
