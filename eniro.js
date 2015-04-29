ba4ece63ceac5c8d5b52f4ee5e1bf3bb12b8ec36
Johan Beronius <johan.beronius@athega.se>
2015-04-29 19:26:31 +0200

    Fixed user scripts to loop and retry until both jQuery and the map are available.

--- eniro.js
+++ eniro.js
@@ -1,16 +1,31 @@
+function addFullscreenButton() {
 
-var $fullscreenButton = $('<div title="Fullskärm" class="e-toggle-button" style="position: absolute; right: 304px; bottom: 26px;"><button class="e-icon">Full</button></div>')
+    if ('jQuery' in window) {
+        var $mapControl = $('.e-full-map');
+        if ($mapControl.length) {
+            var $fullscreenButton = $('<div title="Fullskärm" class="e-toggle-button" style="position: absolute; right: 304px; bottom: 26px;"><button class="e-icon">Full</button></div>')
 
-$fullscreenButton.insertAfter('.e-full-map').click(function(event){
-    var m = document.getElementById('page');
-    m.requestFullscreen = m.requestFullscreen ||
-                          m.mozRequestFullScreen ||
-                          m.webkitRequestFullscreen ||
-                          m.msRequestFullscreen;
-    m.requestFullscreen();
-    event.preventDefault();
-});
+            $fullscreenButton.insertAfter($mapControl).click(function(event){
+                var m = document.getElementById('page');
+                m.requestFullscreen = m.requestFullscreen ||
+                                      m.mozRequestFullScreen ||
+                                      m.webkitRequestFullscreen ||
+                                      m.msRequestFullscreen;
+                m.requestFullscreen();
+                event.preventDefault();
+            });
 
-$(document).on('fullscreenchange mozfullscreenchange webkitfullscreenchange', function(event) {
-    $fullscreenButton.toggle(!(document.mozFullScreen || document.webkitIsFullScreen));
-});
+            $(document).on('fullscreenchange mozfullscreenchange webkitfullscreenchange', function(event) {
+                $fullscreenButton.toggle(!(document.mozFullScreen || document.webkitIsFullScreen));
+            });
+
+            return;
+        }
+    }
+
+    if (--addFullscreenButton.retries)
+        setTimeout(addFullscreenButton, 1500);
+}
+
+addFullscreenButton.retries = 10;
+addFullscreenButton();
--- garmin-connect.js
+++ garmin-connect.js
@@ -1,25 +1,27 @@
 function addFullscreenButton() {
-    var mapControls = jQuery('.map-controls').eq(0).has('.map-control-item');
 
-    if (mapControls.length) {
-        var mapContainer = jQuery('#map_container, #activity-map-canvas')[0];
-        mapContainer.parentElement.style.height = mapContainer.scrollHeight+'px';
-        mapContainer.style.width = mapContainer.style.height = '100%';
-        mapContainer.requestFullscreen = mapContainer.requestFullscreen ||
-          mapContainer.mozRequestFullScreen ||
-          mapContainer.webkitRequestFullscreen ||
-          mapContainer.msRequestFullscreen;
+    if ('jQuery' in window) {
+        var mapControls = jQuery('.map-controls').eq(0).has('.map-control-item');
+        if (mapControls.length) {
+            var mapContainer = jQuery('#map_container, #activity-map-canvas')[0];
+            mapContainer.parentElement.style.height = mapContainer.scrollHeight+'px';
+            mapContainer.style.width = mapContainer.style.height = '100%';
+            mapContainer.requestFullscreen = mapContainer.requestFullscreen ||
+              mapContainer.mozRequestFullScreen ||
+              mapContainer.webkitRequestFullscreen ||
+              mapContainer.msRequestFullscreen;
 
-        var fullscreenButton = jQuery('<button class="map-control-item map-btn map-full-screen" title="Fullscreen"><i class="map-btn-icon icon-full-screen-2"></i></button>');
-        fullscreenButton.click(function() { mapContainer.requestFullscreen() });
-        mapControls.append(fullscreenButton);
-    } else if (--addFullscreenButton.retries)
+            var fullscreenButton = jQuery('<button class="map-control-item map-btn map-full-screen" title="Fullscreen"><i class="map-btn-icon icon-full-screen-2"></i></button>');
+            fullscreenButton.click(function() { mapContainer.requestFullscreen() });
+            mapControls.append(fullscreenButton);
+
+            return;
+        }
+    }
+
+    if (--addFullscreenButton.retries)
         setTimeout(addFullscreenButton, 1500);
 }
 
 addFullscreenButton.retries = 10;
-
-if ('jQuery' in window)
-    addFullscreenButton();
-else
-    window.addEventListener('load', addFullscreenButton);
+addFullscreenButton();
