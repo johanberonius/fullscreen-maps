995669ba289e8b298aa1379b9d118712f1495937
Johan Beronius <johan.beronius@athega.se>
2017-02-20 15:38:44 +0100

    Resize map in fullscreen on Garmin Connect with Safari or Chrome.

--- garmin-connect.js
+++ garmin-connect.js
@@ -1,19 +1,27 @@
 function addFullscreenButton() {
 
     if ('jQuery' in window) {
-        var mapControls = jQuery('.map-controls').eq(0).has('.map-control-item');
-        if (mapControls.length) {
-            var mapContainer = jQuery('#map_container, #activity-map-canvas')[0];
-            mapContainer.parentElement.style.height = mapContainer.scrollHeight+'px';
-            mapContainer.style.width = mapContainer.style.height = '100%';
+        var $mapControls = jQuery('.map-controls').eq(0).has('.map-control-item');
+        if ($mapControls.length) {
+            var $mapContainer = jQuery('#activity-map-canvas'),
+                mapContainer = $mapContainer[0];
+
             mapContainer.requestFullscreen = mapContainer.requestFullscreen ||
               mapContainer.mozRequestFullScreen ||
               mapContainer.webkitRequestFullscreen ||
               mapContainer.msRequestFullscreen;
 
-            var fullscreenButton = jQuery('<button class="map-control-item map-btn map-full-screen" title="Fullscreen"><i class="map-btn-icon icon-full-screen-2"></i></button>');
-            fullscreenButton.click(function() { mapContainer.requestFullscreen() });
-            mapControls.append(fullscreenButton);
+            var $fullscreenButton = jQuery('<button class="map-control-item map-btn map-full-screen" title="Fullscreen"><i class="map-btn-icon icon-full-screen-2"></i></button>');
+            $fullscreenButton.click(function() {
+                mapContainer.requestFullscreen();
+            });
+
+            $(document).on('fullscreenchange mozfullscreenchange webkitfullscreenchange', function(event) {
+                setTimeout(function() { window.dispatchEvent(new Event('resize')); }, 1200);
+            });
+
+            $mapControls.append($fullscreenButton);
+            $mapContainer.before('<style>#activity-map-canvas:-webkit-full-screen {width: 100% !important; height: 100% !important;}</style>');
 
             return;
         }
